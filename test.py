b6f336f6b1650c7b420612d6c0c8a3da2a93e571
septette <127487141+septette@users.noreply.github.com>
2024-07-07 21:00:55 +1000

    Add a runner for OCaml

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ import configparser
 from bs4 import BeautifulSoup
 import os
 import shutil
+from abc import *
+import glob
+import re
+from dataclasses import *
+from enum import *
+import subprocess
+import filecmp
 
 def login(username, password):
     session = requests.Session()
@@ -62,8 +69,11 @@ class Problem:
             for d in dirs:
                 shutil.rmtree(os.path.join(root, d))
 
+    def get_problem_directory(self, root_directory):
+        return os.path.join(root_directory, self.code)
+
     def download_io(self, root_directory):
-        problem_directory = os.path.join(root_directory, self.code)
+        problem_directory = self.get_problem_directory(root_directory)
         if os.path.exists(problem_directory):
             _, _, files = next(os.walk(problem_directory))
             should_delete = input(f'Directory {problem_directory} exists with {len(files)} files inside. Delete files? (y/N) ')
@@ -91,6 +101,90 @@ class Problem:
                 self.save("https://cses.fi" + a.get('href'),
                           os.path.join(problem_directory, f"expected_{i // 3 + 1}.txt"),
                           True)
+    
+    def get_io_pairs(self, root_directory):
+        problem_directory = self.get_problem_directory(root_directory)
+        input_files = glob.glob(os.path.join(problem_directory, 'input_*.txt'))
+        expected_files = set(glob.glob(os.path.join(problem_directory, 'expected_*.txt')))
+        # (i, input, output) tuples (so that they end up sorted)
+        results = []
+        for infile in input_files:
+            match = re.search(r'^.*/input_(.+)\.txt$', infile)
+            if not match: continue
+            i = match.group(1)
+            expected_matches = [e for e in expected_files if re.search(f"^.*/expected_{i}.txt", e)]
+            if len(expected_matches) != 1:
+                print('Found too many expected matches, ignoring:', expected_matches)
+            results.append((i, infile, expected_matches[0]))
+
+        return sorted(results)
+
+class Status(Enum):
+    SUCCESS = 0
+    FAIL = 1
+
+class TerminalColors(Enum):
+    CEND = '\33[0m'
+    CGREENBG = '\33[42m'
+    CREDBG = '\33[41m'
+
+@dataclass
+class Result:
+    i: int
+    result: Status = Status.FAIL
+
+    def show(self):
+        if self.result == Status.SUCCESS:
+            return (TerminalColors.CGREENBG + " [ ✔ ] " +  TerminalColors.CEND + f" Test {self.i}")
+        elif self.result == Status.FAIL:
+            return TerminalColors.CREDBG + " [ ✘ ] " + TerminalColors.CEND + f"Test {self.i}"
+
+class Runner(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.results = []
+        
+    def reset_stats(self):
+        self.results = []
+
+    def compile(self, runfile):
+        self.reset_stats()
+        self.compile_lang(runfile)
+
+    @abstractmethod
+    def compile_lang(self, runfile):
+        raise NotImplementedError("")
+
+    @abstractmethod
+    def get_default_runfile_name(self, code):
+        raise NotImplementedError("")
+    
+    @abstractmethod
+    def run(self, i, input_file, expected_file, runfile):
+        raise NotImplementedError("")
+    
+    def stats(self):
+        for stat in self.results:
+            print(stat.show())
+    
+class OcamlRunner(Runner):
+    def get_default_runfile_name(self, code):
+        return code + '.ml'
+
+    def compile_lang(self, runfile):
+        subprocess.run("ocamlopt -o under_test " + runfile, shell=True)
+
+    def run(self, i, input_file, expected_file, runfile):
+        subprocess.run(f"./under_test < {input_file} > temp_file.txt")
+        # todo - timing etc.
+        status = Status.SUCCESS if filecmp.cmp('temp_file.txt', expected_file) else Status.FAIL
+        self.stats.append(Result(i, status))
+
+class RunnerFactory:
+    @staticmethod
+    def create(lang):
+        if lang == 'ocaml':
+            return OcamlRunner()
 
 def run():
     config = parse_config('config.ini')
@@ -100,8 +194,17 @@ def run():
     lang = config.get('DEFAULT', 'lang')
     session = login(username, password)
 
+    runner = RunnerFactory.create(lang)
+
     code = input("Problem code: ").strip()
     problem = Problem(code, session)
     problem.download_io(directory)
 
+    tentative_filepath = os.path.join(directory, runner.get_default_runfile_name(code))
+    runfile = input(f"Submit file ({tentative_filepath}): ").strip() or tentative_filepath
+    runner.compile(runfile)
+
+    for i, input_file, expected_file in problem.get_io_pairs(directory):
+        runner.run(i, input_file, expected_file)
+    runner.stats()
 run()
